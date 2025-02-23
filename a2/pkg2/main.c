cd161a16f52178d1dc18b433b96888ae0195c5dd
Sean Xiao <guanzhongsean@outlook.com>
2025-02-23 00:50:17 -0500

    Add 2.5 packages for a2

---
+++ a2/pkg2/main.c
@@ -0,0 +1,48 @@
+#include <stddef.h>
+#include <string.h>
+#include <sys/types.h>
+
+/* external interfaces */
+ssize_t read(int fd, void *buffer, size_t count);
+int puts(const char *buffer);
+
+/* exposed interfaces */
+inline __attribute__((always_inline)) ssize_t in(void *buffer, size_t count) {
+  return read(0, buffer, count);
+}
+
+inline __attribute__((always_inline)) int out(const char *buffer) {
+  return puts(buffer);
+}
+
+void abort(void);
+
+void safe_function() { /* Do nothing */ }
+
+#define MAX_INPUT_SIZE 1024
+
+int main(void) {
+  char input[MAX_INPUT_SIZE] = {0};
+  int len = in(input, MAX_INPUT_SIZE);
+
+  void (*crash_func)() = safe_function;
+
+  char c1 = 'A', c2 = 'B';
+  int i = 4;
+  if (len >= 2 && input[0] == c1 && input[1] == c2) {
+    crash_func = abort;
+    c1 = 'C';
+    c2 = 'D';
+    i = 42;
+  }
+
+  if (len == i && input[2] == c1 && input[3] == c2 &&
+      (i == 42
+           ? strcmp(input, "ABCD!Q@#$!@#$#@DA{>_+#$KF<EP{P{#!P(})}}cx,") == 0
+           : 1)) {
+    crash_func();
+  }
+
+  out(input);
+  return 0;
+}
---
+++ a2/pkg3/main.c
@@ -0,0 +1,39 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+/* external interfaces */
+ssize_t read(int fd, void *buffer, size_t count);
+int puts(const char *buffer);
+
+/* exposed interfaces */
+inline __attribute__((always_inline)) ssize_t in(void *buffer, size_t count) {
+  return read(0, buffer, count);
+}
+
+inline __attribute__((always_inline)) int out(const char *buffer) {
+  return puts(buffer);
+}
+
+void abort(void);
+
+#define MAX_INPUT_SIZE 5
+
+int main(void) {
+  char input[MAX_INPUT_SIZE] = {0};
+  int len = in(input, MAX_INPUT_SIZE);
+
+  int a = 0;
+  int *buffer[5] = {&a, &a, &a, &a, NULL};
+  char sentinel = 'A';
+  int cur = 0;
+  for (int i = 0; i < len; i++) {
+    if (input[i] == sentinel) {
+      *buffer[cur++] = sentinel;
+      sentinel += cur * (i + 1) * 2;
+    }
+  }
+  out(input);
+
+  return 0;
+}
